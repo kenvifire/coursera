5df51bfd273cea9142b6db49580f032b7e27a736
kenvifire <mrwhite@163.com>
2013-03-26 22:18:07 +0800

    add files

---
+++ Functional Programming Principles in Scala/neproject/Example/test/scala/example/NewJUnitTest.scala
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package scala.example
+
+import org.junit._
+import Assert._
+
+class NewJUnitTest {
+
+  @Before
+  def setUp: Unit = {
+  }
+
+  @After
+  def tearDown: Unit = {
+  }
+
+  @Test
+  def example = {
+  }
+
+}
---
+++ Natural Language Processing/assignment1/emit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+text_file = open("test")
+
+lines = text_file.readlines()
+
+
+
+
+for line in lines:
+	if line:
+		print None
+	
+	
+
+
+text_file.close()
